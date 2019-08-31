c9162929d53208ea5f13c7be4f27f360127c3d9d
Afzal Shaikh <shaikhafzal9900@gmail.com>
2019-09-01 02:05:08 +0530

    frontend signup and login design created

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router , Route } from 'react-router-dom';
+
+
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
         <Navbar />
-        <Landing />
+        {/* <Landing  */}
+        <Route exact path = "/" component={Landing} />
+        
+        <div className="container">
+        <Route exact path = "/register" component={Register} />
+        <Route exact path = "/login" component={Login} />
+        </div>
+        
+
+
         <Footer />
       </div>
+      </Router>
     );
   }
 }
